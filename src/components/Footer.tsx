6fe5bdb8a4f2291e73f8d70e13c5a438f9307a09
amang9446 <dev.aman7571@gmail.com>
2024-07-15 00:34:50 +0530

    minor refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="flex justify-center flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
