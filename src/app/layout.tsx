858b2b120a2f9a4f0d84d79dbe0c87c5a30adb14
amang9446 <dev.aman7571@gmail.com>
2025-07-11 10:05:57 +0530

    feat: added shadcn and bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,11 +59,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-black`}
       >
-        {children}
+        <div className="fixed inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] pointer-events-none"></div>
+        <div className="fixed left-1/2 top-[-10%] h-[1000px] w-[1000px] -translate-x-1/2 rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)] pointer-events-none"></div>
+
+        <div className="relative z-10">{children}</div>
       </body>
     </html>
   );
