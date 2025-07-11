2bb5b0ef7803c0e296b238b8d98a6e5674f10452
amang9446 <dev.aman7571@gmail.com>
2025-07-11 09:17:25 +0530

    feat: rm-rf old code

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="flex justify-center flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs text-muted-foreground">
-        &copy; {new Date().getFullYear() } Aman Gupta. All rights reserved.
-      </p>
-    </footer>
-  );
-};
